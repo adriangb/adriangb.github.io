fd592a433af9a60bfc50078a2acdb8e98fad1e3b
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2019-05-21 17:27:08 -0700

    Add fading background.

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function recursionJoke() {
+    old_trans = document.getElementById("upButton").style.transition;
+    document.getElementById("upButton").style.transition = "none";
+    document.getElementById("upButton").style.visibility = "hidden";
+    alert("Recursion error: can't link back to self!");
+    document.getElementById("upButton").style.transition = old_trans;
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("upButton").style.visibility = "visible";
+    } else {
+    document.getElementById("upButton").style.visibility = "hidden";
+    }
+    fade_at = 800;  // Scroll point at which background dissapears, in px
+    background_opacity = Math.max((fade_at - Math.max(document.body.scrollTop, document.documentElement.scrollTop)) / fade_at, 0);
+    document.getElementById("background").style.opacity = background_opacity
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    old_trans = document.getElementById("upButton").style.transition;
+    document.getElementById("upButton").style.transition = "none";
+    document.getElementById("upButton").style.visibility = "hidden";
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("upButton").style.transition = old_trans;
+}
